960b056ff872498049b11a59b0b9adb1e9c0238c
Ben <bftingle@ucsc.edu>
2021-11-01 18:37:49 -0700

    Initial Commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public bool canMove;
+    public bool wallGrab;
+    public bool wallJumped;
+    public bool wallSlide;
+    public bool isDashing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/MovementToggler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementToggler : MonoBehaviour
+{
+    public GameObject player;
+
+    public void UseBase() {
+        Behaviour script = player.GetComponent<ImprovedMovement>();
+        if (script != null) script.enabled = false;
+        script = player.GetComponent<BaseMovement>();
+        if (script != null) script.enabled = true;
+        player.GetComponentInChildren<AnimationScript>().UseBase();
+    }
+
+    public void UseImproved() {
+        Behaviour script = player.GetComponent<BaseMovement>();
+        if (script != null) script.enabled = false;
+        script = player.GetComponent<ImprovedMovement>();
+        if (script != null) script.enabled = true;
+        player.GetComponentInChildren<AnimationScript>().UseImproved();
+    }
+}
